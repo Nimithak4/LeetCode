33a3184871a3777c17a2a21761a094928cd52b55
Nimitha K <85860757+Nimithak4@users.noreply.github.com>
2026-07-13 11:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 212.3 MB (42.48%) - LeetSync

---
+++ 1-two-sum/two-sum.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    hash = {}
+    nums.each_with_index do |n, i|
+        diff = target - n
+        if hash.key?(diff)
+            return [hash[diff], i]
+        end
+        hash[n] = i
+    end
+    nil
+end
